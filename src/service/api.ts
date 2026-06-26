0e11c3f4e672fb956e19f570d147c6fcede09eee
Piyumalsha Isurandi <piyumalshaisurandi@gmail.com>
2026-06-26 17:38:43 +0530

    Update API base URL to include versioning

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "https://vendor-backend-kr2j.vercel.app",
+  baseURL: "https://vendor-backend-kr2j.vercel.app/api/v1",
    withCredentials: true,
 })
 
