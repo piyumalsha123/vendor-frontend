ff42524bafaff66e42afb862e1312f83bb0ff5ec
Piyumalsha Isurandi <piyumalshaisurandi@gmail.com>
2026-07-01 15:09:03 +0530

    commit new

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { FaTrash } from 'react-icons/fa';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const AdminDashboard = () => {
   const navigate = useNavigate();
